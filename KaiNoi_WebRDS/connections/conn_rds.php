6e4265ded19f3445296eb4653579a3526f01c982
Samart <30612583+samartka@users.noreply.github.com>
2024-07-08 14:09:43 +0700

    Add files via upload

---
+++ KaiNoi_WebRDS/connections/conn_rds.php
@@ -0,0 +1,21 @@
+
+<?php 
+//เชื่อมต่อ
+ini_set('display_errors', 1);
+error_reporting(~0);
+
+
+$servername = "your_server";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name"; 
+
+// Create connection
+$conn_rds= new mysqli($servername, $username, $password, $dbname);
+ // Change character set to utf8
+ mysqli_set_charset($conn_rds,"utf8");
+// Check connection
+if ($conn_rds->connect_error) {
+    die("Connection failed: " . $conn_rds->connect_error);
+}
+?>
